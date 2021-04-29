dcdc9604e0934fb2307b3016d2ad34f40907cee4
Felippe Chemello <felippe.chemello@kinghost.com.br>
2021-04-29 20:52:26 -0300

    Trocando autenticaçao do rabbitmq

--- src/services/RabbitService.ts
+++ src/services/RabbitService.ts
@@ -6,12 +6,12 @@ export default class RabbitService {
     private channel: Channel
 
     constructor() {
-        if (!process.env.RABBITMQ_URI) {
-            console.error('Error at loading RabbitMQ URI')
+        if (!process.env.RABBITMQ_USER || !process.env.RABBITMQ_PASSWORD || !process.env.RABBITMQ_HOST || !process.env.RABBITMQ_PORT) {
+            console.error('Error at loading RabbitMQ auth')
             process.exit(1)
         }
 
-        this.uri = process.env.RABBITMQ_URI
+        this.uri = `amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`
     }
 
     async start(queue: string): Promise<void> {
